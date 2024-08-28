3a0ddc868ac2c0de744440574e7a8e60efbde61e
ashishshres <ashishshr27@gmail.com>
2024-08-28 18:22:34 +0545

    minor fixes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useId, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { TodoContext } from "../utils/Context";
 import { nanoid } from "nanoid";
 
